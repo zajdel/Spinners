26b82750b390bbd7083825564c57681cbc2e91f3
Jove Yuan <jyuan.twin1@gmail.com>
2017-10-10 23:59:01 -0700

    Initial Algorithm 4 commit

--- Cleanup/1_create_traces.py
+++ Cleanup/1_create_traces.py
@@ -144,8 +144,6 @@ plt.show()
 
 # after closing plot:
 selected_points = selected_points.keys()
-selected_points = list(selected_points)
-selected_points = [list(c) for c in selected_points]
 
 num_selected_points = len(selected_points)
 
@@ -153,119 +151,35 @@ num_selected_points = len(selected_points)
 #################################################################
 #################################################################
 
-# v3
-# 1. Threshold the sdv image (close if necessary) and use Canny edge detection on frames.
-# 2. Take bitwise AND of the threshold and the image stack, result should be "active" edges.
-# 3. For each selected point:
-#     3a. Find the points on the edge.  Define a region of interest (5x5 or 6x6 box) around the selected point and find
-#         all white points in the "active" image.
-#     3b. Take weighted regression of the points on the edge, the center point must lie on the line.  Using the center
-#         point as the origin, the line lies in two of the four quadrants.  Compare each point on the edge to the center,
-#         final angle can be calculated from slope of line and quadrant with most points.
-
 # ret, thresh = cv2.threshold(sdv, 50, 255, cv2.THRESH_BINARY)
 # plt.imshow(sdv.astype(np.uint8))
+thresh_frames = np.asarray([cv2.adaptiveThreshold(f, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 2) for f in frames])
+tifffile.imsave('thresh_frames.tif', np.asarray(thresh_frames))
+
 ret, thresh = cv2.threshold(sdv.astype(np.uint8), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 # plt.imshow(thresh)
 
 num_frames = len(frames)
 frames_and_thresh = [cv2.bitwise_and(frames[i], thresh) for i in range(num_frames)]
-thresh_edges = [cv2.Canny(frame, 100, 250, apertureSize=3) for frame in frames_and_thresh]
-edges = [cv2.Canny(frames[i], 100, 250, apertureSize=3) for i in range(num_frames)]
-tifffile.imsave('edges2000.tif', np.asarray(edges))
+
+thresh_frames = np.asarray([cv2.adaptiveThreshold(f, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 2) for f in frames_and_thresh])
+tifffile.imsave('thresh_frames.tif', np.asarray(thresh_frames))
+
+# plt.imshow(frames_and_thresh[0])
+
+# thresh_edges = [cv2.Canny(frame, 100, 250, apertureSize=3) for frame in frames_and_thresh]
+# edges = [cv2.Canny(frames[i], 100, 250, apertureSize=3) for i in range(num_frames)]
+# tifffile.imsave('edges2000.tif', np.asarray(edges))
 
 
-def plot_ellipses(ellipses):
-    # necessary to redefine fig to show animation?
-    fig, ax = plt.subplots()
-
-    im = ax.imshow(edges[0], aspect='equal')
-
-    ellipse_patch = None
-
-    p1, p2 = None, None
-
-    def init():
-        # preferably nonlocal, but not supported in Python 2
-        global ellipse_patch
-        global p1, p2
-        im.set_data(edges[0])
-        # find first non-None ellipse in ellipses
-        first_ellipse = next(ellipse for ellipse in ellipses if ellipse is not None)
-        ellipse_patch = patches.Ellipse(*first_ellipse, color='r', fill=False)
-        ax.add_patch(ellipse_patch)
-
-        center, width, height, theta = first_ellipse
-        major, minor = width / 2, height / 2
-        p1 = patches.Rectangle(
-            (center[0] - cos(radians(theta)) * major - 0.5, center[1] - sin(radians(theta)) * major - 0.5), 1, 1,
-            color='b', fill=False)
-        p2 = patches.Rectangle(
-            (center[0] + cos(radians(theta)) * major - 0.5, center[1] + sin(radians(theta)) * major - 0.5), 1, 1,
-            color='g', fill=False)
-        ax.add_patch(p1)
-        ax.add_patch(p2)
-
-    def animate(i):
-        global ellipse_patch
-        global p1, p2
-        im.set_data(edges[i % len(edges)])
-        if ellipse_patch:
-            ellipse_patch.remove()
-            ellipse_patch = None
-
-            p1.remove()
-            p2.remove()
-        if ellipses[i % len(edges)]:
-            ellipse_patch = patches.Ellipse(*ellipses[i % len(edges)], color='r', fill=False)
-            ax.add_patch(ellipse_patch)
-
-            center, width, height, theta = ellipses[i % len(edges)]
-            major, minor = width / 2, height / 2
-            p1 = patches.Rectangle((center[0] - cos(radians(theta)) * major - 0.5, center[1] - sin(radians(theta)) * major -0.5), 1, 1, color='b', fill=False)
-            p2 = patches.Rectangle((center[0] + cos(radians(theta)) * major - 0.5, center[1] + sin(radians(theta)) * major - 0.5), 1, 1, color='g', fill=False)
-            ax.add_patch(p1)
-            ax.add_patch(p2)
-        return im
-
-    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(frames), interval=100)
-
-    # Writer = animation.writers['ffmpeg']
-    # writer = Writer(fps=10)
-    # anim.save('ellipses.mp4', writer=writer)
-
-    plt.show()
-
-
-def find_ellipse(point, frame):
+def find_furthest_point(point, frame):
+    # get all points connected to center, find furthest points
     points = Queue()
     points.put(point)
-    fringe = set()
+    cell = set()
+    cell.add(point)
     marked = set()
     marked.add(point)
-
-    start = point
-    # find one pixel on border with floodfill, then perform border traversal
-    if edges[frame][point[1], point[0]] == 0:
-        while not len(fringe):
-            p = points.get()
-            if edges[frame][p[1], p[0]] == 255:
-                fringe.add(p)
-                start = p
-                break
-            p1 = (p[0] - 1, p[1])
-            p2 = (p[0] + 1, p[1])
-            p3 = (p[0], p[1] - 1)
-            p4 = (p[0], p[1] + 1)
-            for p in [p1, p2, p3, p4]:
-                if p not in marked and 0 <= p[0] < len(edges[frame][1]) and 0 <= p[1] < len(edges[frame][0]):
-                    marked.add(p)
-                    points.put(p)
-
-    points = Queue()
-    points.put(start)
-    fringe = set()
-    fringe.add(start)
 
     while not points.empty():
         p = points.get()
@@ -278,85 +192,28 @@ def find_ellipse(point, frame):
         p7 = (p[0] + 1, p[1] - 1)
         p8 = (p[0] + 1, p[1] + 1)
         for p in [p1, p2, p3, p4, p5, p6, p7, p8]:
-            if (p not in fringe
-                    and 0 <= p[0] < len(edges[frame][1])
-                    and 0 <= p[1] < len(edges[frame][0])
-                    and edges[frame][p[1], p[0]] == 255
-                    and frames_and_thresh[i][p[1], p[0]] != 0):
-                fringe.add(p)
+            if (p not in cell
+                    and 0 <= p[0] < len(thresh_frames[frame][1])
+                    and 0 <= p[1] < len(thresh_frames[frame][0])
+                    and p not in marked
+                    and thresh_frames[frame][p[1], p[0]] == 0):
                 points.put(p)
+                cell.add(p)
+        marked.add(point)
 
-    # returns ((x-coordinate of center, y-coordinate of center), (height, width), rotation), width >= height
-    # angle orientation: 0 vertical up, positive clockwise
-    if len(fringe) >= 5:
-        ellipse = cv2.fitEllipse(np.array(list(fringe)))
-    else:
-        # fitEllipse does not work if there are fewer than 5 points on the fringe
-        return None
-    center = ellipse[0]
-    # width >= height
-    minor = ellipse[1][0] / 2 # minor = height / 2
-    major = ellipse[1][1] / 2 # major = width / 2
-    theta = ellipse[2] + 90 # define angle to be zero horizontally right
-
-    return list(fringe), center, major, minor, theta
-
-
-def find_correct_orientation(theta, center, points):
-    score = {theta: 0, (theta + 180) % 360: 0}
-    for p in points:
-        a = np.arctan2(p[1] - center[1], p[0] - center[0])
-        b = np.degrees(a)
-        c = b - theta
-        d = c % 180
-        if (np.degrees(np.arctan2(p[1] - center[1], p[0] - center[0])) - theta) % 180 <= 90:
-            score[theta] += 1
-        else:
-            score[(theta + 180) % 360] += 1
-    return max(score, key=score.get)
+    cell = list(cell)
+    # pairs = [(cell[i], cell[j]) for i in range(len(cell)) for j in range(i + 1, len(cell))]
+    # return max(pairs, key=lambda x: euclidean_distance(*x))
+    return max(cell, key=lambda x: euclidean_distance(x, point))
 
 
 unwrapped = None
-for point in selected_points:
+for center in selected_points:
     ellipses = []
     trace = []
     for i in range(num_frames):
-        ellipse = find_ellipse(tuple(point), i)
-        if ellipse:
-            border, center, major, minor, theta = ellipse
-
-            correction = find_correct_orientation(theta, point, border)
-
-            ellipses.append([center, major * 2, minor * 2, theta])
-            # trace.append(original_theta)
-            # trace.append(radians(original_theta))
-            trace.append(radians(correction % 360))
-        else:
-            ellipses.append(None)
-            trace.append(None)
-
-    if trace[0] is None:
-        # TODO: what to do if first ellipse is undefined
-        trace[0] = next(ang for ang in trace if ang is not None)
-    for i in range(1, len(trace) - 2):
-        if trace[i] is None:
-            # if ellipse does not exist, take previous and next non-None values and linearly approximate
-            try:
-                # TODO: check
-                next_valid_index = next(j for j in range(i, len(trace)) if trace[j] is not None)
-                trace[i] = trace[i - 1] + (trace[next_valid_index] - trace[i - 1]) / (next_valid_index - (i - 1))
-            except StopIteration:
-                prev, next_prev = i, i - 1
-                for j in range(i, -1, -1):
-                    if edges[j] is not None:
-                        prev = j
-                        break
-                for k in range(prev, -1, -1):
-                    if edges[k] is not None:
-                        next_prev = k
-                        break
-                trace[i] = trace[next_prev] + (trace[prev] - trace[next_prev]) / (prev - next_prev) * (i + 1 - prev)
-    plot_ellipses(ellipses)
+        furthest_point = find_furthest_point((center[0], center[1]), i)
+        trace.append(np.arctan2(furthest_point[1] - center[1], furthest_point[0] - center[0]))
 
     unwrapped = np.unwrap(np.asarray(trace))
 
@@ -364,11 +221,30 @@ for point in selected_points:
     plt.ylabel('Angle', fontsize=20)
     plt.title('Trace', fontsize=20)
     plt.plot(unwrapped, 'r-', lw=1)
-    # annotation for 100nM_leu100n_1.tif
-    plt.axvspan(203, 207, color='green', alpha=0.5)
-    plt.axvspan(1656, 1658, color='green', alpha=0.5)
-    plt.axvspan(1662, 1666, color='green', alpha=0.5)
-    plt.axvspan(1824, 1825, color='green', alpha=0.5)
+    # # annotation for 100nM_leu100n_1.tif
+    # plt.axvspan(203, 207, color='green', alpha=0.5)
+    # plt.axvspan(1656, 1658, color='green', alpha=0.5)
+    # plt.axvspan(1662, 1666, color='green', alpha=0.5)
+    # plt.axvspan(1824, 1825, color='green', alpha=0.5)
+
+    # annotation for leu_100um_2.tif (100uM_leu100u_6.tif) (2017-09-22) Point 150, 18
+    plt.axvspan(0, 26, color='green', alpha=0.5)
+    plt.axvspan(146, 168, color='green', alpha=0.5)
+    plt.axvspan(173, 199, color='green', alpha=0.5)
+    plt.axvspan(209, 217, color='green', alpha=0.5)
+    plt.axvspan(240, 273, color='green', alpha=0.5)
+    plt.axvspan(279, 281, color='green', alpha=0.5)
+    plt.axvspan(287, 308, color='green', alpha=0.5)
+    plt.axvspan(331, 383, color='green', alpha=0.5)
+    plt.axvspan(386, 400, color='green', alpha=0.5)
+    plt.axvspan(406, 440, color='green', alpha=0.5)
+    plt.axvspan(441, 482, color='green', alpha=0.5)
+    plt.axvspan(483, 504, color='green', alpha=0.5)
+    plt.axvspan(507, 513, color='green', alpha=0.5)
+    plt.axvspan(516, 535, color='green', alpha=0.5)
+    plt.axvspan(538, 563, color='green', alpha=0.5)
+    plt.axvspan(624, 878, color='green', alpha=0.5)
+
     # plt.plot(trace[:300], 'r-', lw=1)
     # plt.plot(trace, 'bo', markersize=1)
     plt.grid(True, which='both')
