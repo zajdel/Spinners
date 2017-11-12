e6cced073d1d6a07edaa2797af94b0d41384cc5f
Jove Yuan <jyuan.twin1@gmail.com>
2017-11-11 22:19:53 -0800

    Preparation for mass data processing

--- Cleanup/1_create_traces.py
+++ Cleanup/1_create_traces.py
@@ -40,76 +40,9 @@ frames = np.array(raw_frames[0], dtype=np.uint8)
 # *^*^*^*^*^*^*^*^*^*^*^     Getting Donut Image        *^*^*^*^*^*^*^*^*^*^*^*^*^*^
 # *^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^
 
-sdv = np.std(frames[0:15], axis=0)
+sdv = np.mean(frames[0:15], axis=0)
 # rescale sdv and then multiply by (2^N - 1), where N is the depth of each pixel
-sdv = np.divide(np.subtract(sdv, np.amin(sdv)), np.amax(sdv) - np.amin(sdv)) * (2**8 - 1)
-
-plt.imshow(sdv)
-plt.show()
-
-def draw_circles(img, circles):
-    # img = cv2.imread(img,0)
-    cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    for i in circles[0, :]:
-    # draw the outer circle
-        cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
-        # draw the center of the circle
-        cv2.circle(cimg,(i[0], i[1]), 2, (0, 0, 255), 3)
-        cv2.putText(cimg, str(i[0]) + str(',') + str(i[1]), (i[0], i[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.4, 255)
-    return cimg
-
-def detect_circles():
-    gray = sdv.astype(np.uint8)
-    # ret, gray = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
-
-    kernel = np.ones((3, 3), np.uint8)
-
-    gray = cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel)
-    plt.imshow(gray)
-    plt.show()
-    gray = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel)
-    plt.imshow(gray)
-    plt.show()
-
-    # gray_blur = cv2.medianBlur(gray, 3)  # Remove noise before laplacian
-    # gray_lap = cv2.Laplacian(gray_blur, cv2.CV_8UC1, ksize=3)
-    # dilate_lap = cv2.dilate(gray_lap, (3, 3))  # Fill in gaps from blurring. This helps to detect circles with broken edges.
-    # # Furture remove noise introduced by laplacian. This removes false pos in space between the two groups of circles.
-    # lap_blur = cv2.bilateralFilter(dilate_lap, 5, 9, 9)
-    # Fix the resolution to 16. This helps it find more circles. Also, set distance between circles to 55 by measuring dist in image.
-    # Minimum radius and max radius are also set by examining the image.
-    # circles = cv2.HoughCircles(gray_lap, cv2.HOUGH_GRADIENT, 16, 5, param2=15, minRadius=0, maxRadius=9)
-    # for i in range(1, 72, 5):
-    #     for j in range(1, 14, 2):
-    #         for k in range(10, 211, 20):
-    #             circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, i, j, param2=k, maxRadius = 12)
-    #             num_circles = circles[0].shape[0] if circles is not None else 0
-    #             print("dp: {}, minDist: {}, param2: {} gives {} circles".format(i, j, k, num_circles))
-
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 6, 3, param2=10, minRadius=5, maxRadius=12)
-    # 16 2 200 -- 67
-    # 16 4 200 -- 67
-    # 16 8 200 -- 67
-    # 1 4 200 -- 0
-    # 32 4 200 -- 32
-    # 8 4 200 -- 138
-    # 4 4 200 -- 195
-    # 64 4 200 -- 2
-    # 8 4 100 -- 256
-    print("{} circles detected.".format(circles[0].shape[0]))
-    cimg = draw_circles(gray, circles)
-    return cimg
-
-cimg = detect_circles()
-plt.imshow(cimg)
-plt.show()
-
-# featuresize = 11
-# f1=tp.locate(sdv, featuresize)
-#
-# plt.figure()
-# tp.annotate(f1, sdv)
-
+# sdv = np.divide(np.subtract(sdv, np.amin(sdv)), np.amax(sdv) - np.amin(sdv)) * (2**8 - 1)
 
 # *^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^
 # *^*^*^*^*^*^*^*^*^*^*^     Overlay Donut on BG        *^*^*^*^*^*^*^*^*^*^*^*^*^*^
@@ -144,53 +77,25 @@ fig, ax = plt.subplots()
 im = ax.imshow(show_frames[0], aspect='equal')
 
 # Points automatically chosen
-# selected_points = set()
-# def on_press(event):
-#     if event.xdata and event.ydata:
-#         x, y = int(round(event.xdata)), int(round(event.ydata))
-#         print('You pressed {0} at ({1}, {2}) with sdv value of {3}.'.format(event.button, x, y, sdv[y, x]))
-#
-#         rect = patches.Rectangle((x - 0.5, y - 0.5), 1, 1, linewidth=1, edgecolor='r', facecolor='none')
-#         ax.add_patch(rect)
-#         correct_x, correct_y = min([(i, j) for i in range(x - 1, x + 2) for j in range(y - 1, y + 2)
-#                                     if 0 < i < sdv.shape[1] and 0 < j < sdv.shape[0]], key=lambda p: sdv[p[::-1]])
-#         selected_points.add(
-#             (correct_x, correct_y)  # use set to avoid duplicates being stored. (use tuple because can hash.)
-#         )
-#         if correct_x != x or correct_y != y:
-#             print('Corrected green box at ({0}, {1})'.format(correct_x, correct_y))
-#             area = patches.Rectangle((x - 2.5, y - 2.5), 5, 5, linewidth=0.5, edgecolor='r', facecolor='none')
-#             correction = patches.Rectangle((correct_x - 0.5, correct_y - 0.5), 1, 1, linewidth=0.5, edgecolor='g', facecolor='none')
-#             ax.add_patch(area)
-#             ax.add_patch(correction)
-#     else:
-#         pass
-
-# Points not automatically chosen, click to remove
-selected_points = {}
+selected_points = set()
 def on_press(event):
     if event.xdata and event.ydata:
-        # Note that numpy arrays are accessed by [row (y), col(x)], but images are indexed by [x, y]
         x, y = int(round(event.xdata)), int(round(event.ydata))
         print('You pressed {0} at ({1}, {2}) with sdv value of {3}.'.format(event.button, x, y, sdv[y, x]))
 
-        if (x, y) not in selected_points:
-            rect = patches.Rectangle((x - 0.5, y - 0.5), 1, 1, linewidth=0.5, edgecolor='r', facecolor='none')
-            selected_points[(x, y)] = rect
-
-            # Add the patch to the Axes
-            ax.add_patch(rect)
-
-            # correct_x, correct_y = min([(i, j) for i in range(x - 1, x + 2) for j in range(y - 1, y + 2)
-            #                             if 0 < i < sdv.shape[1] and 0 < j < sdv.shape[0]], key=lambda p: sdv[p[::-1]])
-            # if correct_x != x or correct_y != y:
-            #     print('Corrected green box at ({0}, {1})'.format(correct_x, correct_y))
-            #     area = patches.Rectangle((x - 2.5, y - 2.5), 5, 5, linewidth=0.5, edgecolor='r', facecolor='none')
-            #     correction = patches.Rectangle((correct_x - 0.5, correct_y - 0.5), 1, 1, linewidth=0.5, edgecolor='g', facecolor='none')
-            #     ax.add_patch(area)
-            #     ax.add_patch(correction)
-        else:
-            selected_points.pop((x, y)).remove()
+        rect = patches.Rectangle((x - 0.5, y - 0.5), 1, 1, linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+        correct_x, correct_y = min([(i, j) for i in range(x - 2, x + 3) for j in range(y - 2, y + 3)
+                                    if 0 < i < sdv.shape[1] and 0 < j < sdv.shape[0]], key=lambda p: sdv[p[::-1]])
+        selected_points.add(
+            (correct_x, correct_y)  # use set to avoid duplicates being stored. (use tuple because can hash.)
+        )
+        if correct_x != x or correct_y != y:
+            print('Corrected green box at ({0}, {1})'.format(correct_x, correct_y))
+            area = patches.Rectangle((x - 2.5, y - 2.5), 5, 5, linewidth=0.5, edgecolor='r', facecolor='none')
+            correction = patches.Rectangle((correct_x - 0.5, correct_y - 0.5), 1, 1, linewidth=0.5, edgecolor='g', facecolor='none')
+            ax.add_patch(area)
+            ax.add_patch(correction)
     else:
         pass
 
@@ -210,9 +115,6 @@ def animate(i):
 anim = animation.FuncAnimation(fig, animate, init_func=init, interval=100)
 plt.show()
 
-# after closing plot:
-selected_points = selected_points.keys()
-
 num_selected_points = len(selected_points)
 
 #################################################################
@@ -221,30 +123,8 @@ num_selected_points = len(selected_points)
 
 num_frames = len(frames)
 
-# # Median filter with 3x3 kernel
-# frames = [cv2.medianBlur(f, 3) for f in frames]
-#
-# # Local Otsu threshold with radius 15
-# radius = 15
-# selem = disk(radius)
-# # frames = [(f >= rank.otsu(f, selem)) * 255 for f in frames]
-# for i in range(num_frames):
-#     start = time.time()
-#     frames[i] = (frames[i] >= rank.otsu(frames[i], selem)) * 255
-#     print("Frames %i: %f seconds elapsed." % (i, time.time() - start))
-#
-# tifffile.imsave('local_otsu.tif', np.asarray(frames))
-#
-# # Standard deviation of local thresholded Otsu image
-# sdv = np.std(frames, axis=0)
-# # rescale sdv and then multiply by (2^N - 1), where N is the depth of each pixel
-# sdv = np.divide(np.subtract(sdv, np.amin(sdv)), np.amax(sdv) - np.amin(sdv)) * (2**8 - 1)
-# sdv = (sdv >= threshold_otsu(sdv)) * 255
-#
-# # Take bitwise AND of SDV mask and thresholded Otsu frames
+# Take bitwise AND of SDV mask and thresholded Otsu frames
 # frames_and_sdv = [cv2.bitwise_and(frames[i].astype(np.uint8), sdv.astype(np.uint8)) for i in range(num_frames)]
-#
-# tifffile.imsave('thresh_frames.tif', np.asarray(frames_and_sdv))
 
 
 def find_furthest_points(center, frame):
@@ -304,8 +184,8 @@ for center in selected_points:
         trace.append(ang)
 
     # unwrap trace and apply 1D median filter (default kernel size 3)
-    unwrapped = medfilt(np.unwrap(np.asarray(trace)))
-    traces.append(unwrapped)
+    unwrapped = medfilt(np.unwrap(np.asarray(trace[2:])))
+    traces.append(np.insert(unwrapped, 0, [center[0], center[1]]))
 
     plt.xlabel('Frame', fontsize=20)
     plt.ylabel('Angle', fontsize=20)
@@ -380,4 +260,4 @@ for center in selected_points:
     # plt.savefig("leu_100u_6_speed.png")
     plt.show()
 
-np.savetxt("traces.csv", np.asarray(traces), delimiter=",")
+np.savetxt(fname + ".csv", np.asarray(traces), delimiter=",", fmt="%.4f")
