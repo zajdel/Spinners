Fixed issue with mean calculation

Cleanup/1_create_traces.py
@@ -40,7 +40,7 @@ frames = np.array(raw_frames[0], dtype=np.uint8)
 # *^*^*^*^*^*^*^*^*^*^*^     Getting Donut Image        *^*^*^*^*^*^*^*^*^*^*^*^*^*^
 # *^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^
 
-sdv = np.mean(frames[0:15], axis=0)
+sdv = np.mean(frames[0:500], axis=0)
 # rescale sdv and then multiply by (2^N - 1), where N is the depth of each pixel
 # sdv = np.divide(np.subtract(sdv, np.amin(sdv)), np.amax(sdv) - np.amin(sdv)) * (2**8 - 1)
 
@@ -86,8 +86,8 @@ def on_press(event):
         rect = patches.Rectangle((x - 0.5, y - 0.5), 1, 1, linewidth=1, edgecolor='r', facecolor='none')
         ax.add_patch(rect)
         roi = [(i, j) for i in range(x - 2, x + 3) for j in range(y - 2, y + 3) if 0 < i < sdv.shape[1] and 0 < j < sdv.shape[0]]
-        min_intensity = min([sdv[p[::-1]] for p in roi]) # p[::-1] simply reverses tuple
-        correct_x, correct_y = np.mean([v for i, v in enumerate(roi) if v == min_intensity], axis=0)
+        min_intensity = min([sdv[p[::-1]] for p in roi]) # p[::-1] reverses tuple
+        correct_x, correct_y = np.mean([p for p in roi if sdv[p[::-1]] == min_intensity], axis=0)
         selected_points.add(
             (correct_x, correct_y)  # use set to avoid duplicates being stored. (use tuple because can hash.)
         )