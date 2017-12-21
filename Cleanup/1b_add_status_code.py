b758c1344d2bd0e7feb85f092f373554620de8f2
Jove Yuan <jyuan.twin1@gmail.com>
2017-12-20 22:20:30 -0800

    Minor changes

--- Cleanup/1b_add_status_code.py
+++ Cleanup/1b_add_status_code.py
@@ -1,11 +1,11 @@
 from utilities import *
 #  Ex. python 1b_add_status_code.py 100nM_leu100n_1 [Centers + Trace in CSV]
-# Used to add status codes/flags (-1: unverified, 0: verified - bad, 1: verified - good) to older data
+# Script is used to add status codes/flags (-1: unverified, 0: verified - bad, 1: verified - good) to older data
 
 fname = sys.argv[1]
 dataname = fname + '.csv'
 data = np.loadtxt(dataname, delimiter=",")
-centers, trace = np.hsplit(data, np.array([2, 3]))
+centers, trace = np.hsplit(data, 2)
 
 status = np.zeros((data.shape[0], 1), dtype=np.int)
 new_data = np.hstack((centers, status, trace))
