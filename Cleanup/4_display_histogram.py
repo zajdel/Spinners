ded06f9fd0dc18cf123df9c055abe33000401813
zajdel <tjzajdel@gmail.com>
2018-02-01 01:25:52 -0800

    revamp display histogram
    to plot multiple distribtuions from one condition over a few time points - still needs work

--- Cleanup/4_display_histogram.py
+++ Cleanup/4_display_histogram.py
@@ -6,14 +6,33 @@ from utilities import *
 import pandas as pd
 
 csv = sys.argv[1] # csv to make histogram from
-concentration = sys.argv[2]
+
+colors = ['b', 'g', 'r', 'c', 'm', 'k']
 
 # data = pd.read_csv(csv + '.csv', sep=',', header=None)
-data = np.loadtxt(csv + '.csv', delimiter=',')
 
-plt.hist(data, bins=20, range=(0, 1))
-plt.ylabel('Frequency')
-plt.xlabel('Bias')
-plt.title('Bias of ' + concentration)
+csv = ['10u_leu_30s','10u_leu_60s','10u_leu_120s']
+
+for k in range(0,len(csv)):
+    data = np.loadtxt(csv[k] + '.csv', delimiter=',')
+    plt.subplot(221)
+    plt.hist(data[:,0], bins=20, range=(0,1), normed=1,facecolor=colors[k], alpha=0.25)
+    plt.xlabel('Bias')
+    plt.ylabel('Frequency')
+    plt.subplot(222)
+    plt.hist(data[:,1], bins=20, range=(0,30), normed=1,facecolor=colors[k], alpha=0.25)
+    plt.xlabel('$\tau_{ccw}$')
+    plt.ylabel('Frequency')
+    plt.subplot(223)
+    plt.hist(data[:,2], bins=20, range=(0,30), normed=1,facecolor=colors[k], alpha=0.25)
+    plt.xlabel('$\tau_{cw}$')
+    plt.ylabel('Frequency')
+    plt.subplot(224)
+    plt.hist(data[:,3], bins=20, range=(0,100), normed=1,facecolor=colors[k], alpha=0.25)
+    plt.xlabel('$N_s$')
+    plt.ylabel('Frequency')
+
+
+
 
 plt.show()
